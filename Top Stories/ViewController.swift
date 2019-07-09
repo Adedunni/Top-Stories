f83404ec49c4b2be8e1b6c0f80ff4ea1a777363d
Dunni <andlewis14@gmail.com>
2019-07-09 10:11:24 -0500

    Initial Commit

---
+++ Top Stories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by Dunni Dayo - Kayode on 7/9/19.
+//  Copyright © 2019 Dunni Dayo - Kayode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view
+    }
+
+
+}
+
